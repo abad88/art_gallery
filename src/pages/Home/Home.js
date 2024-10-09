03c1cf71d7ad824635dea816785da6ea6e5f35f6
Abad Siddiqui <abadsid775@gmail.com>
2024-10-09 18:46:02 -0400

    about us material and logo added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Home.css"
 import contactBanner from "../../assets/images/arth.jpg"
 import Slider from '../../components/Slider/Slider'
+import Logo from "../../assets/images/logo.jpg"
 
 const Home = () => {
     return (
@@ -10,7 +11,8 @@ const Home = () => {
                 <div className="contactBanner">
                     <img src={contactBanner} alt="logo" />
                     <div className="heading">
-                        <h1>24 CPS Art Gallery</h1>
+                        {/* <h1>24 CPS Art Gallery</h1> */}
+                        <img src={Logo} alt="logo" className='logo' />
                     </div>
                 </div>
                 <div className='contentHome'>
