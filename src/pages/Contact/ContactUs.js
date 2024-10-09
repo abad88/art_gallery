396ddcb02d20b0fa405cecd25209ac3385d875b0
Abad Siddiqui <abadsid775@gmail.com>
2024-10-09 16:25:17 -0400

    contact us page changes

--- src/pages/Contact/ContactUs.js
+++ src/pages/Contact/ContactUs.js
@@ -12,11 +12,26 @@ const ContactUs = () => {
                 </div>
             </div>
             <h1 className='text-center'>Fine Art Gallery in New York</h1>
-            <h3>24 Central Park South, New York, New York 10019, United States</h3>
-            <h3>Tel: 212-588-8700</h3>
-            <h3>Phone/Whatsapp/Wechat: 917-470-5061</h3>
-            <h3>Email: 24cpsgallery@gmail.com</h3>
-            <h3>Opening Hours: Monday-Sunday 10:00AM - 07:00PM</h3>
+            <div className='contentBorder'>
+                <div className='contactContent'>
+                    <h2>Location</h2>
+                    <h3>24 Central Park South, New York, New York 10019, United States</h3>
+                </div>
+
+                <div className='contactContent'>
+                    <h2>Contact Information</h2>
+                    <h3>Tel: 212-588-8700</h3>
+                    <h3>Phone/Whatsapp/Wechat: 917-470-5061</h3>
+                    <h3>Email: 24cpsgallery@gmail.com</h3>
+                </div>
+
+                <div className='contactContent'>
+                    <h2>Opening Hours</h2>
+                    <h3>Monday-Sunday 10:00AM - 07:00PM</h3>
+                </div>
+
+            </div>
+
         </div>
     )
 }
