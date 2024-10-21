734d6f22f98737e4446d9d30bc087c2a00df0a6d
abad88 <abad_sid@outlook.com>
2024-10-21 16:03:28 -0400

    data added in home decor

--- src/pages/DecCat/HomeDecor.js
+++ src/pages/DecCat/HomeDecor.js
@@ -4,22 +4,77 @@ import contactBanner from "../../assets/images/decor.jpg"
 import LazyLoad from 'react-lazyload'; // Import LazyLoad for lazy loading
 import img1 from "../../assets/images/decor.jpg"
 import { useNavigate } from 'react-router'
+import vase1 from "../../assets/images/vases/vase1/main3.png"
+import vase2 from "../../assets/images/vases/vase2/main3.png"
+import vase3 from "../../assets/images/vases/vase3/main2.png"
+import vase4 from "../../assets/images/vases/vase4/main1.png"
+import vase5 from "../../assets/images/vases/vase5/main1.png"
+import vase6 from "../../assets/images/vases/vase6/main1.png"
+import vase7 from "../../assets/images/vases/vase7/main1.png"
+import vase8 from "../../assets/images/vases/vase8/main1.png"
+import vase9 from "../../assets/images/vases/vase9/main1.png"
+import vase10 from "../../assets/images/vases/vase10/main1.png"
+import vase11 from "../../assets/images/vases/vase11/main1.png"
+import vase12 from "../../assets/images/vases/vase12/main1.png"
+import vase13 from "../../assets/images/vases/vase13/main1.png"
+import vase14 from "../../assets/images/vases/vase14/main1.png"
+import vase15 from "../../assets/images/vases/vase15/main1.png"
+import vase16 from "../../assets/images/vases/vase16/main1.png"
+import vase17 from "../../assets/images/vases/vase17/main1.png"
+import vase18 from "../../assets/images/vases/vase18/main1.png"
+import vase19 from "../../assets/images/vases/vase19/main1.png"
+import vase20 from "../../assets/images/vases/vase20/main1.png"
+import vase21 from "../../assets/images/vases/vase21/main1.png"
+import vase22 from "../../assets/images/vases/vase22/main1.png"
+import vase23 from "../../assets/images/vases/vase23/main1.png"
+import vase24 from "../../assets/images/vases/vase24/main1.png"
+import vase25 from "../../assets/images/vases/vase25/main1.png"
+import vase26 from "../../assets/images/vases/vase26/main1.png"
+import clock1 from "../../assets/images/clocks/clock1/main1.png"
+import clock2 from "../../assets/images/clocks/clock2/main1.png"
+import clock3 from "../../assets/images/clocks/clock3/main1.png"
+import table1 from "../../assets/images/tables/table1/main1.jpg"
+import table2 from "../../assets/images/tables/table2/main1.png"
+import lamp1 from "../../assets/images/lamp/lamp1/main1.png"
+import lamp2 from "../../assets/images/lamp/lamp2/main1.png"
 
 const HomeDecor = () => {
     const navigate = useNavigate();
     const imageData = [
-        { img: img1, title: 'Image 1', artist: "artist1", type: "3D" },
-        { img: img1, title: 'Image 2', artist: "artist2", type: "3D" },
-        { img: img1, title: 'Image 3', artist: "artist1", type: "3D" },
-        { img: img1, title: 'Image 4', artist: "artist1", type: "3D" },
-        { img: img1, title: 'Image 5', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 6', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 7', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 8', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 9', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 10', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 11', artist: "artist3", type: "3D" },
-        { img: img1, title: 'Image 12', artist: "artist3", type: "3D" },
+        { img: vase1, title: 'Vase1', artist: "artist2", type: "Vase" },
+        { img: vase2, title: 'Vase2', artist: "artist2", type: "Vase" },
+        { img: vase3, title: 'Vase3', artist: "artist2", type: "Vase" },
+        { img: vase4, title: 'Vase4', artist: "artist2", type: "Vase" },
+        { img: vase5, title: 'Vase5', artist: "artist3", type: "Vase" },
+        { img: vase6, title: 'Vase6', artist: "artist3", type: "Vase" },
+        { img: vase7, title: 'Vase7', artist: "artist3", type: "Vase" },
+        { img: vase8, title: 'Vase8', artist: "artist3", type: "Vase" },
+        { img: vase9, title: 'Vase9', artist: "artist3", type: "Vase" },
+        { img: vase10, title: 'Vase10', artist: "artist3", type: "Vase" },
+        { img: vase11, title: 'Vase11', artist: "artist3", type: "Vase" },
+        { img: vase12, title: 'Vase12', artist: "artist3", type: "Vase" },
+        { img: vase13, title: 'Vase13', artist: "artist3", type: "Vase" },
+        { img: vase14, title: 'Vase14', artist: "artist3", type: "Vase" },
+        { img: vase15, title: 'Vase15', artist: "artist3", type: "Vase" },
+        { img: vase16, title: 'Vase16', artist: "artist3", type: "Vase" },
+        { img: vase17, title: 'Vase17', artist: "artist3", type: "Vase" },
+        { img: vase18, title: 'Vase18', artist: "artist3", type: "Vase" },
+        { img: vase19, title: 'Vase19', artist: "Marble", type: "Vase" },
+        { img: vase20, title: 'Vase20', artist: "artist3", type: "Vase" },
+        { img: vase21, title: 'Vase21', artist: "Marble", type: "Vase" },
+        { img: vase22, title: 'Vase22', artist: "artist3", type: "Vase" },
+        { img: vase23, title: 'Vase23', artist: "artist3", type: "Vase" },
+        { img: vase24, title: 'Vase24', artist: "artist3", type: "Vase" },
+        { img: vase25, title: 'Vase25', artist: "artist3", type: "Vase" },
+        { img: vase26, title: 'Vase26', artist: "artist3", type: "Vase" },
+        { img: clock1, title: 'Clock1', artist: "artist3", type: "Clock" },
+        { img: clock2, title: 'Clock2', artist: "artist3", type: "Clock" },
+        { img: clock3, title: 'Clock3', artist: "artist3", type: "Clock" },
+        { img: lamp1, title: 'lamp1', artist: "artist3", type: "Lamp" },
+        { img: lamp2, title: 'lamp2', artist: "artist3", type: "Lamp" },
+        { img: table1, title: 'Table1', artist: "artist3", type: "Table" },
+        { img: table2, title: 'Table2', artist: "artist3", type: "Table" },
+
 
         // Add more images as needed
     ];
@@ -84,17 +139,18 @@ const HomeDecor = () => {
                 />
 
                 <select value={selectedArtist} onChange={handleArtistChange} className="filterSelect">
-                    <option value="All">All Artists</option>
-                    <option value="artist1">Artist1</option>
+                    <option value="All">Material</option>
+                    <option value="Marble">Marble</option>
                     <option value="artist2">Artist2</option>
                     <option value="artist3">Artist3</option>
                 </select>
 
                 <select value={selectedType} onChange={handleTypeChange} className="filterSelect">
                     <option value="All">All Types</option>
-                    <option value="3D">3D Art</option>
-                    <option value="2D">2D Art</option>
-                    <option value="4D">4D Art</option>
+                    <option value="Vase">Vase</option>
+                    <option value="Clock">Clock</option>
+                    <option value="Table">Table</option>
+                    <option value="Lamp">Lamp</option>
                 </select>
 
                 <div className="imagesGrid">
--- src/pages/HomeDec/HomeDecPage.js
+++ src/pages/HomeDec/HomeDecPage.js
@@ -6,6 +6,72 @@ import Button from '../../components/Button/Button';
 import im1 from "../../assets/images/artpiece/eka_22.jpg";
 import im2 from "../../assets/images/artpiece/eka_5.jpg";
 import Form from '../../components/Form/Form';
+import vase1_1 from "../../assets/images/vases/vase1/main2.png"
+import vase1_2 from "../../assets/images/vases/vase1/main1.png"
+import vase2_1 from "../../assets/images/vases/vase1/main2.png"
+import vase2_2 from "../../assets/images/vases/vase1/main1.png"
+import vase3_1 from "../../assets/images/vases/vase1/main3.png"
+import vase3_2 from "../../assets/images/vases/vase1/main1.png"
+import vase4_1 from "../../assets/images/vases/vase1/main2.png"
+import vase4_2 from "../../assets/images/vases/vase1/main3.png"
+import vase5_1 from "../../assets/images/vases/vase1/main2.png"
+import vase5_2 from "../../assets/images/vases/vase1/main3.png"
+import vase6_1 from "../../assets/images/vases/vase1/main2.png"
+import vase6_2 from "../../assets/images/vases/vase1/main3.png"
+import vase7_1 from "../../assets/images/vases/vase1/main2.png"
+import vase7_2 from "../../assets/images/vases/vase1/main3.png"
+import vase8_1 from "../../assets/images/vases/vase1/main2.png"
+import vase8_2 from "../../assets/images/vases/vase1/main3.png"
+import vase9_1 from "../../assets/images/vases/vase1/main2.png"
+import vase9_2 from "../../assets/images/vases/vase1/main3.png"
+import vase10_1 from "../../assets/images/vases/vase1/main2.png"
+import vase10_2 from "../../assets/images/vases/vase1/main3.png"
+import vase11_1 from "../../assets/images/vases/vase1/main2.png"
+import vase11_2 from "../../assets/images/vases/vase1/main3.png"
+import vase12_1 from "../../assets/images/vases/vase1/main2.png"
+import vase12_2 from "../../assets/images/vases/vase1/main3.png"
+import vase13_1 from "../../assets/images/vases/vase1/main2.png"
+import vase13_2 from "../../assets/images/vases/vase1/main3.png"
+import vase14_1 from "../../assets/images/vases/vase1/main2.png"
+import vase14_2 from "../../assets/images/vases/vase1/main3.png"
+import vase15_1 from "../../assets/images/vases/vase1/main2.png"
+import vase15_2 from "../../assets/images/vases/vase1/main3.png"
+import vase16_1 from "../../assets/images/vases/vase1/main2.png"
+import vase16_2 from "../../assets/images/vases/vase1/main3.png"
+import vase17_1 from "../../assets/images/vases/vase1/main2.png"
+import vase17_2 from "../../assets/images/vases/vase1/main3.png"
+import vase18_1 from "../../assets/images/vases/vase1/main2.png"
+import vase18_2 from "../../assets/images/vases/vase1/main3.png"
+import vase19_1 from "../../assets/images/vases/vase1/main2.png"
+import vase19_2 from "../../assets/images/vases/vase1/main3.png"
+import vase20_1 from "../../assets/images/vases/vase1/main2.png"
+import vase20_2 from "../../assets/images/vases/vase1/main3.png"
+import vase21_1 from "../../assets/images/vases/vase1/main2.png"
+import vase21_2 from "../../assets/images/vases/vase1/main3.png"
+import vase22_1 from "../../assets/images/vases/vase1/main2.png"
+import vase22_2 from "../../assets/images/vases/vase1/main3.png"
+import vase23_1 from "../../assets/images/vases/vase1/main2.png"
+import vase23_2 from "../../assets/images/vases/vase1/main3.png"
+import vase24_1 from "../../assets/images/vases/vase1/main2.png"
+import vase24_2 from "../../assets/images/vases/vase1/main3.png"
+import vase25_1 from "../../assets/images/vases/vase1/main2.png"
+import vase25_2 from "../../assets/images/vases/vase1/main3.png"
+import vase26_1 from "../../assets/images/vases/vase1/main2.png"
+import vase26_2 from "../../assets/images/vases/vase1/main3.png"
+import clock1_1 from "../../assets/images/clocks/clock1/main2.png"
+import clock1_2 from "../../assets/images/clocks/clock1/main3.png"
+import clock2_1 from "../../assets/images/clocks/clock2/main2.png"
+import clock2_2 from "../../assets/images/clocks/clock2/main3.png"
+import clock3_1 from "../../assets/images/clocks/clock3/main2.png"
+import clock3_2 from "../../assets/images/clocks/clock3/main3.png"
+import lamp1_1 from "../../assets/images/lamp/lamp1/main2.png"
+import lamp1_2 from "../../assets/images/lamp/lamp1/main3.png"
+import lamp2_1 from "../../assets/images/lamp/lamp2/main2.png"
+import lamp2_2 from "../../assets/images/lamp/lamp2/main3.png"
+import table1_1 from "../../assets/images/tables/table1/main2.jpg"
+import table1_2 from "../../assets/images/tables/table1/main3.jpg"
+import table2_1 from "../../assets/images/tables/table2/main2.jpg"
+import table2_2 from "../../assets/images/tables/table2/main3.jpg"
 
 const HomeDecPage = () => {
     const location = useLocation();
@@ -35,14 +101,310 @@ const HomeDecPage = () => {
             detail: "It is all about Yellow money"
         },
         {
-            titleImg: "Eka 3D Art 1",
+            titleImg: "Vase1",
             author: "Eka Peradze",
             detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
             thumbnails: [
-                im1,
-                im2,
+                vase1_1,
+                vase1_2,
             ]
-        }
+        },
+        {
+            titleImg: "Vase2",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase2_1,
+                vase2_2,
+            ]
+        },
+        {
+            titleImg: "Vase3",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase3_1,
+                vase3_2,
+            ]
+        },
+        {
+            titleImg: "Vase4",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase4_1,
+                vase4_2,
+            ]
+        },
+        {
+            titleImg: "Vase5",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase5_1,
+                vase5_2,
+            ]
+        },
+        {
+            titleImg: "Vase6",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase6_1,
+                vase6_2,
+            ]
+        },
+        {
+            titleImg: "Vase7",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase7_1,
+                vase7_2,
+            ]
+        },
+        {
+            titleImg: "Vase8",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase8_1,
+                vase8_2,
+            ]
+        },
+        {
+            titleImg: "Vase9",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase9_1,
+                vase9_2,
+            ]
+        },
+        {
+            titleImg: "Vase10",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase10_1,
+                vase10_2,
+            ]
+        },
+        {
+            titleImg: "Vase11",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase11_1,
+                vase11_2,
+            ]
+        },        {
+            titleImg: "Vase12",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase12_1,
+                vase12_2,
+            ]
+        },
+        {
+            titleImg: "Vase13",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase13_1,
+                vase13_2,
+            ]
+        },
+        {
+            titleImg: "Vase14",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase14_1,
+                vase14_2,
+            ]
+        },
+        {
+            titleImg: "Vase15",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase15_1,
+                vase15_2,
+            ]
+        },
+        {
+            titleImg: "Vase16",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase16_1,
+                vase16_2,
+            ]
+        },
+        {
+            titleImg: "Vase17",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase17_1,
+                vase17_2,
+            ]
+        },
+        {
+            titleImg: "Vase18",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase18_1,
+                vase18_2,
+            ]
+        },
+        {
+            titleImg: "Vase19",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase19_1,
+                vase19_2,
+            ]
+        },
+        {
+            titleImg: "Vase20",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase20_1,
+                vase20_2,
+            ]
+        },
+        {
+            titleImg: "Vase21",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase21_1,
+                vase21_2,
+            ]
+        },
+        {
+            titleImg: "Vase22",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase22_1,
+                vase22_2,
+            ]
+        },
+        {
+            titleImg: "Vase23",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase23_1,
+                vase23_2,
+            ]
+        },
+        {
+            titleImg: "Vase23",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase23_1,
+                vase23_2,
+            ]
+        },
+        {
+            titleImg: "Vase24",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase24_1,
+                vase24_2,
+            ]
+        },
+        {
+            titleImg: "Vase25",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase25_1,
+                vase25_2,
+            ]
+        },
+        {
+            titleImg: "Vase26",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                vase26_1,
+                vase26_2,
+            ]
+        },
+        {
+            titleImg: "Clock1",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                clock1_1,
+                clock1_2,
+            ]
+        },
+        {
+            titleImg: "Clock2",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                clock2_1,
+                clock2_2,
+            ]
+        },
+        {
+            titleImg: "Clock3",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                clock3_1,
+                clock3_2,
+            ]
+        },
+        {
+            titleImg: "Table1",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                table1_1,
+                table1_2,
+            ]
+        },
+        {
+            titleImg: "Table2",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                table2_1,
+                table2_2,
+            ]
+        },
+        {
+            titleImg: "lamp1",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                lamp1_1,
+                lamp1_2,
+            ]
+        },
+        {
+            titleImg: "lamp2",
+            author: "Eka Peradze",
+            detail: "Eka Peradze's 3D paintings are a captivating blend of art and illusion. Her meticulous craftsmanship and masterful use of light and shadow create stunningly realistic scenes that seem to defy the boundaries of the canvas.",
+            thumbnails: [
+                lamp2_1,
+                lamp2_2,
+            ]
+        },
     ];
 
     // Find the matching art piece based on the title and titleImg comparison
